b86981e8d13fa230f50b24c33798653f3959f946
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-08-06 23:13:04 +0800

    nernst potential

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,11 @@
+# Constants
+const R = 8314      # [J/kmol*K]
+const Frdy = 96485  # [C/mol]
+const Temp = 310    # [K] 310 K (37 C) for BT / 295 K (22 C) for RT
+const FoRT = Frdy/R/Temp
+const VT = inv(FoRT) # Thermal temparature (mV)
+const Qpow = (Temp-310)/10 # Temp factor
+
 # Utility functions
 
 """Hill function"""
@@ -12,3 +20,7 @@ exprel(x) = ifelse(isapprox(x, 0), one(x/expm1(x)), x/expm1(x))
 
 """Logistic function"""
 expit(x) = 1 / (1 + exp(-x))
+
+"""Nernst potential"""
+nernst(xo, xi) = VT * log(xo/xi)
+nernst(xo, xi, z) = nernst(xo, xi) / z
