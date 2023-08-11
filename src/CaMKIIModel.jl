1f2404d6ad4b5f390ed4b467b4a4a5b294984c28
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-08-11 17:24:39 +0800

    include

--- src/CaMKIIModel.jl
+++ src/CaMKIIModel.jl
@@ -5,5 +5,8 @@ export make_b1ar_rn
 include("utils.jl")
 include("bar.jl")
 include("cam.jl")
+include("ions/ica_markov.jl")
+include("ions/ina_hh.jl")
+include("ions/ina_markov.jl")
 
 end # module CaMKIIModel
