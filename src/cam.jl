dbda2f4379649d159434567cef3b69e77b69fb25
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-08-11 16:25:25 +0800

    camkii model

--- src/cam.jl
+++ src/cam.jl
@@ -3,46 +3,47 @@
 using Catalyst
 using ModelingToolkit
 
-function cam_rn(;
-    Mg=1.0,  # mM
-    K=135.0, # mM
+function make_cam_rn(;
+    Mg=1.0,  # Magnesium (mM)
+    K=135.0, # Potassium (mM)
     CaMKII=120.0, # Dyad: 120; SL:120*8.293e-4; Cyt:120*8.293e-4
     PP1tot=96.5, # Dyad: 96.5; SL:0.57; Cyt:0.57
-
 )
-    function kbt(Pb, Pt, Pt2, Pa, CaMKIItot)
-        T = (Pb + Pt + Pt2 + Pa) / CaMKIItot
+    function kbt(Pb, Pt, Pt2, Pa)
+        T = Pb + Pt + Pt2 + Pa
         return 0.055*T + 0.0074*T^2 + 0.015*T^3
     end
 
     rn = @reaction_network begin
+        @variables Ca(t)
+
         # CaM: Calmodulin
         # CaN: Calcineurin
         # B: buffer
-        (k02, k20), CaM + 2Ca <--> Ca2CaM
-        (k24, k42), Ca2CaM + 2Ca <--> Ca4CaM
-        (k02B, k20B), CaMB + 2Ca <--> Ca2CaMB
-        (k24B, k42B), Ca2CaMB + 2Ca <--> Ca4CaMB
+        (k02 * Ca^2, k20), CaM <--> Ca2CaM
+        (k24 * Ca^2, k42), Ca2CaM <--> Ca4CaM
+        (k02B * Ca^2, k20B), CaMB <--> Ca2CaMB
+        (k24B * Ca^2, k42B), Ca2CaMB <--> Ca4CaMB
         (k0Bon, k0Boff), CaM + B <--> CaMB
         (k2Bon, k2Boff), Ca2CaM + B <--> Ca2CaMB
         (k4Bon, k4Boff), Ca4CaM + B <--> Ca4CaMB
-        (kcanCaon, kcanCaoff), Ca2CaN + 2Ca <--> Ca4CaN
-        (k02can, k20can), CaMCa4CaN + 2Ca <--> Ca2CaMCa4CaN
-        (k24can, k42can), Ca2CaMCa4CaN + 2Ca <--> Ca4CaMCa4CaN
+        (kcanCaon * Ca^2, kcanCaoff), Ca2CaN <--> Ca4CaN
+        (k02can * Ca^2, k20can), CaMCa4CaN <--> Ca2CaMCa4CaN
+        (k24can * Ca^2, k42can), Ca2CaMCa4CaN <--> Ca4CaMCa4CaN
         (kcanCaM0on, kcanCaM0off), CaM + Ca4CaN <--> CaMCa4CaN
-        (kcanCaM0on, kcanCaM0off), Ca2CaM + Ca4CaN <--> Ca2CaMCa4CaN
-        (kcanCaM0on, kcanCaM0off), Ca4CaM + Ca4CaN <--> Ca4CaMCa4CaN
+        (kcanCaM2on, kcanCaM2off), Ca2CaM + Ca4CaN <--> Ca2CaMCa4CaN
+        (kcanCaM4on, kcanCaM4off), Ca4CaM + Ca4CaN <--> Ca4CaMCa4CaN
         # CaMKII
         (kib2, kb2i), Ca2CaM + Pi <--> Pb2
-        (kb24, kb42), Pb2 + 2Ca <--> Pb
+        (kb24 * Ca^2, kb42), Pb2 <--> Pb
         (kib, kbi), Ca4CaM + Pi <--> Pb
-        kbt(Pb, Pt, Pt2, Pa, CaMKIItot), Pb --> Pt
-        mm(Pt, kpp1 * PP1tot, Kmpp1), Pt => Pb
-        (kt42, kt24), Pt + 2Ca <--> Pt2
+        kbt(Pb, Pt, Pt2, Pa), Pb --> Pt
+        kpp1*PP1tot*Pt/(Kmpp1+CaMKIItot*Pt), Pt => Pb
+        (kt42 * Ca^2, kt24), Pt <--> Pt2
         (kta, kat), Pt <--> Ca4CaM + Pa
         (kt2a, kat2), Pt2 <--> Ca2CaM + Pa
-        mm(Pt2, kpp1 * PP1tot, Kmpp1), Pt2 => Pb2
-        mm(Pa, kpp1 * PP1tot, Kmpp1), Pa => Pi
+        kpp1*PP1tot*Pt2/(Kmpp1+CaMKIItot*Pt2), Pt2 => Pb2
+        kpp1*PP1tot*Pa/(Kmpp1+CaMKIItot*Pa), Pa => Pi
     end
 
     # Calculate Ca/CaM parameters
@@ -110,7 +111,7 @@ function cam_rn(;
         :k42 => k42,
         :k24 => k24,
         :k0Boff => k0Boff,
-        :k0Bon => k0Bon2,
+        :k0Bon => k0Bon,
         :k2Boff => k2Boff,
         :k2Bon => k2Bon,
         :k4Boff => k4Boff,
@@ -150,3 +151,35 @@ function cam_rn(;
 
     return rn
 end
+
+# adjusting Ca buffering in EC coupling model
+# I don'treally want to repeat this but I'm still waiting for options
+function ca_buf_cam_eq(sys)
+    @unpack Ca, k02, k20, k24, k42, CaM, Ca2CaM, Ca4CaM = sys
+    rcn02 = k02*Ca^2*CaM - k20*Ca2CaM
+    rcn24 = k24*Ca^2*Ca2CaM - k42*Ca4CaM
+
+    @unpack k02B, k20B, k24B, k42B, CaMB, Ca2CaMB, Ca4CaMB = sys
+    rcn02B = k02B*Ca^2*CaMB - k20B*Ca2CaMB
+    rcn24B = k24B*Ca^2*Ca2CaMB - k42B*Ca4CaMB
+
+    @unpack kcanCaon, kcanCaoff, Ca2CaN, Ca4CaN = sys
+    rcnCa4CaN = kcanCaon*Ca^2*Ca2CaN - kcanCaoff*Ca4CaN
+
+    @unpack k02can, k20can, k24can, k42can, CaMCa4CaN, Ca2CaMCa4CaN, Ca4CaMCa4CaN = sys
+    rcn02CaN = k02can*Ca^2*CaMCa4CaN - k20can*Ca2CaMCa4CaN
+    rcn24CaN = k24can*Ca^2*Ca2CaMCa4CaN - k42can*Ca4CaMCa4CaN
+
+    @unpack kb24, kb42, Pb2, Pb, kt42, kt24, Pt, Pt2 = sys
+    rcnCKb2b = kb24*Ca^2*Pb2 - kb42*Pb
+    rcnCKtt2 = kt42*Pt - kt24*Ca^2*Pt2
+
+    @unpack CaMKIItot = sys
+
+    return -2*(CaMKIItot*(-rcnCKtt2+rcnCKb2b) - (rcn02+rcn24+rcn02B+rcn24B+rcnCa4CaN+rcn02CaN+rcn24CaN))
+end
+
+
+cam_rn = make_cam_rn()
+
+ca_buf_cam_eq(cam_rn)
