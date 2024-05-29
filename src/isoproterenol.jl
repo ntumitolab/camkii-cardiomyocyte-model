c94dbe61f61156aed6b12af0921dc997b43abe8f
Wen Wei Tseng <sosiristseng@gmail.com>
2024-05-29 15:10:52 +0800

    get_bar_sys

--- src/isoproterenol.jl
+++ src/isoproterenol.jl
@@ -2,7 +2,7 @@
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
-function get_bar_eqs(ATP=5000μM, ISO=0μM,)
+function get_bar_eqs(ATP=5000μM, ISO=0μM)
     @parameters begin
         b1ARtot = 0.00528μM
         Gstot = 3.83μM
@@ -215,4 +215,9 @@ function get_bar_eqs(ATP=5000μM, ISO=0μM,)
     odeeqs = [D(x) ~ rates[x] for x in (LR, LRG, RG, GsaGTP, GsaGDP, Gsby, b1AR_S464, b1AR_S301, AC_GsaGTP, PDEp, cAMP, RCcAMP_I, RCcAMPcAMP_I, RcAMPcAMP_I, PKACI, PKACI_PKI, RCcAMP_II, RCcAMPcAMP_II, RcAMPcAMP_II, PKACII, PKACII_PKI, I1p, I1p_PP1, PLBp, PLMp, TnIp, LCCap, LCCbp, KURp)]
 
     return [conservedeqs; propeqs; odeeqs]
+end
+
+function get_bar_sys(ATP=5000μM, ISO=0μM; name=:barsys)
+    eqs = get_bar_eqs(ATP, ISO)
+    return ODESystem(eqs, t; name)
 end
