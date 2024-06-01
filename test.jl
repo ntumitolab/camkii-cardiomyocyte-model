9a95540ef3474b81e3f184d8ce9fb089f961d3e1
Wen Wei Tseng <sosiristseng@gmail.com>
2024-06-01 08:25:40 +0000

    could i use catalyst now

--- test.jl
+++ test.jl
@@ -1,10 +1,34 @@
-using OrdinaryDiffEq
-using DiffEqCallbacks
-using ModelingToolkit
-using ModelingToolkit: t_nounits as t, D_nounits as D
-using Plots
-using CaMKIIModel: build_neonatal_ecc_sys
+using Catalyst, OrdinaryDiffEq, Plots, ModelingToolkit
 
-sys = build_neonatal_ecc_sys()
+# set the growth rate to 1.0
+@parameters λ = -1.0
+
+# set the initial volume to 1.0
+@variables t V(t) = 1.0
+
+# build the ODESystem for dV/dt
+D = Differential(t)
+eq = [D(V) ~ λ * V]
+@named osys = ODESystem(eq, t)
+
+# build the ReactionSystem with no protein initially
+rn = @reaction_network begin
+    @species P(t) = 1.0 Q(t) = 0.0
+    $V, P --> Q
+end
+
+conservationlaw_constants(rn)
+
+rnsys = convert(ODESystem, rn, remove_conserved = true)
+
+sys = extend(osys, rnsys)
 
 simp = structural_simplify(sys)
+
+prob = ODEProblem(simp, [])
+
+ts = (0, 10)
+
+sol = solve(prob, Tsit5(); tspan=ts)
+
+plot(sol)
