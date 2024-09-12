00eebea260b7206e5256018789dd847faa4a44f9
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-12 16:38:57 +0800

    test code

--- test.jl
+++ test.jl
@@ -1,19 +1,20 @@
-using CaMKIIModel
+using ProgressLogging
 using ModelingToolkit
 using OrdinaryDiffEq
-using BenchmarkTools
 using Plots
+using CaMKIIModel
 
 sys = build_neonatal_ecc_sys(simplify=true)
+tend = 10.0
+prob = ODEProblem(sys, [], tend, jac=true, sparse=true)
+probnojac = ODEProblem(sys, [], tend)
 
-prob = ODEProblem(sys, [], float(10))
+@unpack Istim = sys
+callback = build_stim_callbacks(Istim, tend)
+alg = Rodas5P()
+@time sol = solve(prob, Rodas5P(); callback, progress=true)
+@time sol = solve(probnojac, Rodas5P(); callback, progress=true)
 
-# TODO: fix unstable (ICaL?)
-sol = solve(prob, Rodas5P())
-
-sol[sys.ICaL]
-
-plot(sol, idxs=sys.PO1RyR)
 
 for (k, v) in zip(unknowns(sys), sol[end])
     println(k, " => ", v)
