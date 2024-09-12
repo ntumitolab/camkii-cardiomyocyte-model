2e9d22656441e752d8f756aa0b6c070eaf11ee0d
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-12 17:13:19 +0800

    Rodas5P wins

--- test.jl
+++ test.jl
@@ -6,15 +6,11 @@ using CaMKIIModel
 
 sys = build_neonatal_ecc_sys(simplify=true)
 tend = 10.0
-prob = ODEProblem(sys, [], tend, jac=true, sparse=true)
-probnojac = ODEProblem(sys, [], tend)
+prob = ODEProblem(sys, [], tend)
 
 @unpack Istim = sys
 callback = build_stim_callbacks(Istim, tend)
-alg = Rodas5P()
 @time sol = solve(prob, Rodas5P(); callback, progress=true)
-@time sol = solve(probnojac, Rodas5P(); callback, progress=true)
-
 
 for (k, v) in zip(unknowns(sys), sol[end])
     println(k, " => ", v)
