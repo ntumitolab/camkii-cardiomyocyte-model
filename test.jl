d81c868c79a44dba36471115e387d210072986d5
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-17 00:15:18 +0800

    test code

--- test.jl
+++ test.jl
@@ -11,11 +11,11 @@ prob = ODEProblem(sys, [], tend)
 
 @unpack Istim = sys
 callback = build_stim_callbacks(Istim, tend)
-@time sol = solve(prob, TRBDF2(); callback, progress=true, abstol=1e-7, reltol=1e-7)
+@time sol = solve(prob, Rodas5P(); callback, progress=true, abstol=1e-7, reltol=1e-7)
 
 plot(sol, idxs=[sys.vm*1000])
 
-plot(sol, idxs=[sys.CaJSR, sys.CaNSR, sys.Cai_sub_SR])
-
-plot(sol, idxs=[sys.PO1RyR])
-@unpack Cai_sub_SR = sys
+plot(sol, idxs=[sys.CaJSR, sys.CaNSR])
+plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL])
+# RyR instability?
+plot(sol, idxs=[sys.betaSR])
