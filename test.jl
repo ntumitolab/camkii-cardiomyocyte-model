5709e2e11ece5138a8b339189ba700452628049d
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-19 21:55:32 +0800

    test code

--- test.jl
+++ test.jl
@@ -1,6 +1,7 @@
 using ProgressLogging
 using ModelingToolkit
-using DifferentialEquations
+using OrdinaryDiffEq
+using DiffEqCallbacks
 using Plots
 using CaMKIIModel
 Plots.default(lw=2)
@@ -10,15 +11,11 @@ tend = 1000.0
 prob = ODEProblem(sys, [], tend)
 
 @unpack Istim = sys
-callback = build_stim_callbacks(Istim, tend)
+callback = build_stim_callbacks(Istim, tend; period=0.5)
 alg = FBDF()
 @time sol = solve(prob, alg; callback, progress=true, abstol=1e-6, reltol=1e-6, maxiters=Int(1e8))
 
-
-plot(sol, idxs=sys.vm, tspan=(90, 100))
-plot(sol, idxs=[sys.CaJSR, sys.CaNSR], tspan=(90, 100))
-plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL, sys.Cai_mean], tspan=(99, 100))
+plot(sol, idxs=sys.vm, tspan=(998, 1000))
+plot(sol, idxs=[sys.CaJSR, sys.CaNSR], tspan=(998, 1000))
+plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL, sys.Cai_mean], tspan=(998, 1000))
 plot(sol, idxs=sys.Jtr)
-
-u0 = sol[end]
-@time sol = solve(remake(prob, u0=u0), alg; progress=true, abstol=1e-9, reltol=1e-9)
