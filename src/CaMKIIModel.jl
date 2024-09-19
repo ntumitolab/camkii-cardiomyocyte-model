6103e8d6458246831fecd755417bba9a5f982451
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-19 19:36:14 +0800

    minimal deps

--- src/CaMKIIModel.jl
+++ src/CaMKIIModel.jl
@@ -3,7 +3,8 @@ module CaMKIIModel
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using NaNMath
-using DifferentialEquations
+using OrdinaryDiffEq
+using DiffEqCallbacks
 
 export get_camkii_sys, get_bar_sys, build_neonatal_ecc_sys, build_stim_callbacks
 
