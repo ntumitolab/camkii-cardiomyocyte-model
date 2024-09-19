1977feb157a9c4d7034fe0f47b0949e6e10e72f7
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-19 17:02:36 +0800

    test code

--- test.jl
+++ test.jl
@@ -5,22 +5,20 @@ using Plots
 using CaMKIIModel
 Plots.default(lw=2)
 
-sys = build_neonatal_ecc_sys(dx=4e-7, simplify=true)
-tend = 30.0
+sys = build_neonatal_ecc_sys(simplify=true)
+tend = 1000.0
 prob = ODEProblem(sys, [], tend)
 
 @unpack Istim = sys
 callback = build_stim_callbacks(Istim, tend)
 alg = FBDF()
-@time sol = solve(prob, alg; callback, progress=true, abstol=1e-7, reltol=1e-7, maxiters=1e8)
+@time sol = solve(prob, alg; callback, progress=true, abstol=1e-6, reltol=1e-6, maxiters=Int(1e8))
 
-plot(sol, idxs=[sys.CaJSR, sys.CaNSR])
-plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL])
-# Sub SR Ca instability even there's no external stimuli
-# Jup unstable? PDE unstable?
-@unpack JCa_SR, V_sub_SR= sys
-plot(sol, idxs=[sys.JCa_SR/V_sub_SR])
-plot(sol, idxs=[sys.Jup, sys.Jrel])
+
+plot(sol, idxs=sys.vm, tspan=(90, 100))
+plot(sol, idxs=[sys.CaJSR, sys.CaNSR], tspan=(90, 100))
+plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL, sys.Cai_mean], tspan=(99, 100))
+plot(sol, idxs=sys.Jtr)
 
 u0 = sol[end]
 @time sol = solve(remake(prob, u0=u0), alg; progress=true, abstol=1e-9, reltol=1e-9)
