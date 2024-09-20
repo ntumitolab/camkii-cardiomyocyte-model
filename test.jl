e917b3bf21fc575e398a386e993a5826d377676d
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-20 17:33:35 +0800

    tets code

--- test.jl
+++ test.jl
@@ -15,6 +15,21 @@ callback = build_stim_callbacks(Istim, tend; period=1)
 alg = FBDF()
 @time sol = solve(prob, alg; callback, progress=true, abstol=1e-6, reltol=1e-6, maxiters=Int(1e8))
 
-plot(sol, idxs=sys.vm, tspan=(995, 1000))
-plot(sol, idxs=[sys.Cai_sub_SR, sys.Cai_sub_SL, sys.Cai_mean], tspan=(998, 1000))
-plot(sol, idxs=sys.CaMKAct)
+# Add ISO
+prob2 = remake(prob, p=[sys.ISO => 1E-5])
+@time sol2 = solve(prob2, alg; callback, progress=true, abstol=1e-6, reltol=1e-6, maxiters=Int(1e8))
+
+plot(sol, idxs=sys.Cai_mean, tspan=(999, 1000), lab="ISO=0", title="Ca transient")
+plot!(sol2, idxs=sys.Cai_mean, tspan=(999, 1000), lab="ISO=0.1")
+
+plot(sol, idxs=sys.PKACI, lab="ISO=0", title="PKACI")
+plot!(sol2, idxs=sys.PKACI, lab="ISO=0.1")
+
+plot(sol, idxs=sys.cAMP, lab="ISO=0", title="cAMP")
+plot!(sol2, idxs=sys.cAMP, lab="ISO=0.1")
+
+@unpack LRG, RG, Gs, GsaGDP, GsaGTP, AC_GsaGTP = sys
+plot(sol2, idxs = [LRG, RG, Gs], tspan=(0, 1))
+
+@unpack b1AR, LR, LRG, RG, b1AR_S464, b1AR_S301 = sys
+plot(sol2, idxs = [b1AR, LR, LRG, RG, b1AR_S464, b1AR_S301], tspan=(0, 10), size=(800, 800))
