cc69a71e2851dc6cd073a75907d16ad42050f66f
Wen Wei Tseng <sosiristseng@gmail.com>
2024-09-26 17:05:13 +0800

    camk sensitivity

--- docs/camk-sens.jl
+++ docs/camk-sens.jl
@@ -5,9 +5,9 @@ using Plots
 using LsqFit
 using CaMKIIModel
 using CaMKIIModel: μM, hil
-Plots.default(lw=2)
 
 # ## Setup CaMKII system
+# Model CaM/Calcium binding only. NO phosphorylation or oxidation.
 @parameters Ca=0μM ROS=0μM
 sys = get_camkii_sys(Ca; ROS, phospho_rate=0, simplify=true)
 
@@ -16,25 +16,48 @@ tend=100.0
 prob = ODEProblem(sys, [], tend)
 alg = Rodas5()
 callback = TerminateSteadyState()
-ca = range(0.0, 10μM, length=1001)
+ca = exp10.(range(log10(1e-2μM), log10(10μM), length=1001))
 prob_func = (prob, i, repeat) -> begin
     remake(prob, p=[Ca => ca[i]])
 end
 trajectories = length(ca)
-sol = solve(prob, alg; callback) ## warmup
+sol0 = solve(remake(prob, p=[Ca => 0.0]), alg; callback) ## warmup
 sim = solve(EnsembleProblem(prob; prob_func, safetycopy=false), alg; trajectories, callback)
 
 """Extract values from ensemble simulations by a symbol"""
 extract(sim, k) = map(s->s[k][end], sim)
 
-plot(ca .* 1000, extract(sim, sys.CaM0), lab="CaM0", ylabel="Conc. (mM)", xlabel="Ca (μM)")
+
+# Components
+plot(ca .* 1000, extract(sim, sys.CaM0), lab="CaM0", ylabel="Conc. (mM)", xlabel="Ca (μM)", xscale=:log10)
 plot!(ca .* 1000, extract(sim, sys.Ca2CaM_C), lab="Ca2CaM_C")
 plot!(ca .* 1000, extract(sim, sys.Ca2CaM_N), lab="Ca2CaM_N")
 plot!(ca .* 1000, extract(sim, sys.Ca4CaM), lab="Ca4CaM")
+plot!(ca .* 1000, extract(sim, sys.CaMK), lab="CaMK")
 plot!(ca .* 1000, extract(sim, sys.CaM0_CaMK), lab="CaM0_CaMK")
 plot!(ca .* 1000, extract(sim, sys.Ca2CaM_C_CaMK), lab="Ca2CaM_C_CaMK")
 plot!(ca .* 1000, extract(sim, sys.Ca2CaM_N_CaMK), lab="Ca2CaM_N_CaMK")
-plot!(ca .* 1000, extract(sim, sys.Ca4CaM_CaMK), lab="Ca4CaM_CaMK")
+plot!(ca .* 1000, extract(sim, sys.Ca4CaM_CaMK), lab="Ca4CaM_CaMK", legend=:topleft, size=(800, 800))
+
+# Active CaMKII
+println("Basal activity when Ca=0 is ", sol0[sys.CaMKAct][end])
+plot(ca .* 1000, extract(sim, sys.CaMKAct), lab="Active CaMKII", xlabel="Ca (μM)", xscale=:log10)
+
+# ## Least-square fitting of CaMKII activity
+hil_s(x, k, n) = sign(x*k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
+@. model_camk(x, p) = p[1] * hil_s(x, p[2], p[3]) + p[4]
+xdata = ca
+ydata = extract(sim, sys.CaMKAct)
+
+p0 = [0.4, 1e-3, 3.0, 0.01]
+lb = [0.0, 0.0, 0.1, 0.0]
+
+fit = curve_fit(model_camk, xdata, ydata, p0; lower=lb)
+pestim = coef(fit)
 
 #---
-plot(ca .* 1000, extract(sim, sys.Ca4CaM_CaMK), lab="Active CaMKII")
+confidence_inter = confint(fit; level=0.95)
+
+#---
+plot(xdata, ydata, lab="Data", line=:dash, title="CaMKII activity", xscale=:log10)
+plot!(x-> model_camk(x, pestim), xdata, lab="Fitted", line=:dot, legend=:topleft)
