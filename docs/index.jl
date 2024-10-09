8613e9d39cdb4d8c81738f579ada2aed12ddff23
Wen Wei Tseng <sosiristseng@gmail.com>
2024-10-09 16:31:06 +0800

    Runtime information

--- docs/index.jl
+++ docs/index.jl
@@ -1,3 +1,4 @@
+#===
 # CaMKII cardiomyocyte model
 
 - https://github.com/drgrandilab/Morotti-et-al-2014-Mouse-Ventricular-Model
@@ -12,3 +13,13 @@ A novel computational model of mouse myocyte electrophysiology to assess the syn
 between Na+ loading and CaMKII.
 J Physiol. 2014 Mar 15;592(6):1181-97.
 doi: https://doi.org/10.1113/jphysiol.2013.266676
+===#
+
+# ## Runtime information
+
+using InteractiveUtils
+InteractiveUtils.versioninfo()
+
+# Packages
+using Pkg
+Pkg.status()
