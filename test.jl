d7c79a26170fba9c2fdf0c1ef6e6fd1430033866
Wen Wei Tseng <sosiristseng@gmail.com>
2024-10-16 17:23:18 +0800

    test code
    under 15% error

--- test.jl
+++ test.jl
@@ -7,3 +7,60 @@ using LsqFit
 using CaMKIIModel
 using CaMKIIModel: μM
 Plots.default(lw=2)
+
+@parameters Ca=0μM ROS=0μM
+sys = get_camkii_sys(Ca; ROS, phospho_rate=0, simplify=true)
+
+tend=100.0
+prob = ODEProblem(sys, [], tend)
+alg = Rodas5()
+callback = TerminateSteadyState()
+ca = exp10.(range(log10(1e-2μM), log10(10μM), length=101))
+prob_func = (prob, i, repeat) -> begin
+    remake(prob, p=[Ca => ca[i]])
+end
+trajectories = length(ca)
+sol0 = solve(remake(prob, p=[Ca => 0.0]), alg; callback) ## warmup
+sim = solve(EnsembleProblem(prob; prob_func, safetycopy=false), alg; trajectories, callback)
+
+extract(sim, k) = map(s->s[k][end], sim)
+hil_s(x, k, n) = sign(x*k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
+sqrt_s(x) = sign(x) * sqrt(abs(x))
+
+basalActivity = sol0[sys.CaMKAct][end]
+plot(ca .* 1000, extract(sim, sys.CaMKAct), lab="Active CaMKII", xlabel="Ca (μM)", xscale=:log10)
+
+xdata = ca
+ydata = extract(sim, sys.CaMKAct)
+
+# CaM + CaMK + 4Ca <---> CaMCa4_CaMKII
+function model(x, p)
+    A = 30μM
+    B = 70μM
+    kmca = p[1]
+    nca = p[2]
+    kfb = p[3]
+    kca = p[4]
+    k0 = 0.021/A
+    y = map(x) do ca
+        keq = kfb * hil_s(ca, kmca, nca) + kca * ca + k0
+        xterm = A + B + 1/keq
+        camkcam4 = 0.5 * ( xterm - sqrt_s(xterm^2 - 4 * A * B))
+        y = camkcam4 / B
+    end
+end
+
+p0 = [1μM, 2.0, 1e7, 1000.0]
+lb = [0.0, 1.0, 0.0, 0.0]
+
+fit = curve_fit(model, xdata, ydata, p0; lower=lb)
+pestim = coef(fit)
+
+confidence_inter = confint(fit; level=0.95)
+
+yestim = model.(xdata, Ref(pestim))
+
+plot(xdata .* 1000, [ydata yestim], lab=["Truth" "Pred"], xlabel="Ca (μM)", xscale=:log10)
+
+# 15% error
+plot(xdata, (yestim .- ydata) ./ ydata .* 100, xscale=:log10)
