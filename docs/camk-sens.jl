d9d01297785527c4e22b9d21b7af6f7794b3a74e
Wen Wei Tseng <sosiristseng@gmail.com>
2024-10-17 10:35:35 +0800

    format

--- docs/camk-sens.jl
+++ docs/camk-sens.jl
@@ -9,11 +9,11 @@ using CaMKIIModel: μM, hil
 
 # ## Setup CaMKII system
 # Model CaM/Calcium binding only. NO phosphorylation or oxidation.
-@parameters Ca=0μM ROS=0μM
+@parameters Ca = 0μM ROS = 0μM
 sys = get_camkii_sys(Ca; ROS, phospho_rate=0, simplify=true)
 
 #---
-tend=10000.0
+tend = 10000.0
 prob = ODEProblem(sys, [], tend)
 alg = Rodas5()
 callback = TerminateSteadyState()
@@ -26,7 +26,7 @@ sol0 = solve(remake(prob, p=[Ca => 0.0]), alg; callback) ## warmup
 sim = solve(EnsembleProblem(prob; prob_func, safetycopy=false), alg; trajectories, callback)
 
 """Extract values from ensemble simulations by a symbol"""
-extract(sim, k) = map(s->s[k][end], sim)
+extract(sim, k) = map(s -> s[k][end], sim)
 
 
 # Components
@@ -45,7 +45,7 @@ println("Basal activity when Ca=0 is ", sol0[sys.CaMKAct][end])
 plot(ca .* 1000, extract(sim, sys.CaMKAct), lab="Active CaMKII", xlabel="Ca (μM)", xscale=:log10)
 
 # ## Least-square fitting of steady state CaMKII activity
-hil_s(x, k, n) = sign(x*k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
+hil_s(x, k, n) = sign(x * k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
 @. model_camk(x, p) = p[1] * hil_s(x, p[2], p[3]) + 0.021
 xdata = ca
 ydata = extract(sim, sys.CaMKAct)
@@ -68,8 +68,8 @@ plot(xdata, (yestim .- ydata) ./ ydata .* 100, title="Error", xscale=:log10)
 
 # ## Least-square fitting of kinetic CaMKII activity
 # CaM + CaMK + 4Ca <---> CaMCa4_CaMKII
-extract(sim, k) = map(s->s[k][end], sim)
-hil_s(x, k, n) = sign(x*k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
+extract(sim, k) = map(s -> s[k][end], sim)
+hil_s(x, k, n) = sign(x * k) * (abs(x)^n) / (abs(x)^n + abs(k)^n)
 sqrt_s(x) = sign(x) * sqrt(abs(x))
 
 xdata = ca
@@ -85,8 +85,8 @@ function model(x, p)
     k0 = p[5]
     y = map(x) do ca
         keq = kfb * hil_s(ca, kmca, nca) + kca * ca + k0
-        xterm = A + B + 1/keq
-        camkcam4 = 0.5 * ( xterm - sqrt_s(xterm^2 - 4 * A * B))
+        xterm = A + B + 1 / keq
+        camkcam4 = 0.5 * (xterm - sqrt_s(xterm^2 - 4 * A * B))
         y = camkcam4 / B
     end
 end
