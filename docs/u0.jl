21cd2fe49a26cd98d4fd675f6d7662b1eef1e12b
Wen Wei Tseng <sosiristseng@gmail.com>
2024-11-01 09:37:01 +0800

    use SteadyStateDiffEq

--- docs/u0.jl
+++ docs/u0.jl
@@ -2,22 +2,18 @@
 # No stimulation for 50000 seconds.
 using ModelingToolkit
 using OrdinaryDiffEq
-using DiffEqCallbacks
+using SteadyStateDiffEq
 using Plots
 using CaMKIIModel
 Plots.default(lw=2)
 
 sys = build_neonatal_ecc_sys(simplify=true, reduce_iso=true)
 tend = 50000.0
-prob = ODEProblem(sys, [], tend)
-alg = FBDF()
-callback = TerminateSteadyState()
+prob = SteadyStateProblem(sys, [])
+alg = DynamicSS(Rodas5P())
 
-sol = solve(prob, alg; callback, abstol=1e-6, reltol=1e-6)
+sol = solve(prob, alg; abstol=1e-8, reltol=1e-8)
 
-unknowns(sys)
-sol.u[end]
-
-for (k, v) in zip(unknowns(sys), sol.u[end])
+for (k, v) in zip(unknowns(sys), sol.u)
     println(k, " => ", v , ",")
 end
