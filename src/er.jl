cfd9455736ac55b510afab910d54bc955ec79471
Wen Wei Tseng <sosiristseng@gmail.com>
2024-11-07 16:32:06 +0800

    wip

--- src/er.jl
+++ src/er.jl
@@ -37,7 +37,7 @@ function get_ser_sys(Cai_sub_SR; fracPLB_CKp=0, fracPLBp=0, RyR_CKp=0.2, V_sub_S
     fCKII_PLB = (1 - 0.5 * fracPLB_CKp)  # Max effect: fCKII_PLB=0.5
     fPKA_PLB = ((1 - fracPLBp) / fracPKA_PLBo) * (1 - 0.5531) + 0.5531
     # Select the smaller value (resulting in max reduction of Kmf)
-    Kmfp = 2 * KmfSR * min(fCKII_PLB, fPKA_PLB)  #fCKII_PLB
+    Kmfp = KmfSR * min(fCKII_PLB, fPKA_PLB)  #fCKII_PLB
     fSR = (Cai_sub_SR / Kmfp)^2
     rSR = (CaNSR / KmrSR)^2
     kleak = (1 + 5 * RyR_CKp) * kSRleak / 2
--- test.jl
+++ test.jl
@@ -5,13 +5,13 @@ using DiffEqCallbacks
 using Plots
 using LsqFit
 using CaMKIIModel
-using CaMKIIModel: μM, Hz
+using CaMKIIModel: μM, Hz, μm
 Plots.default(lw=1.5)
 
 # ## Setup the ODE system
 sys = build_neonatal_ecc_sys(simplify=true, reduce_iso=true)
 tend = 300.0
-prob = ODEProblem(sys, [], tend, [sys.kRyR=>200Hz, sys.VmaxSR=>5])
+prob = ODEProblem(sys, [], tend, [sys.ktrCaSR => 5])
 @unpack Istim = sys
 alg = FBDF()
 
@@ -23,12 +23,16 @@ sol = solve(prob, alg; callback, abstol=1e-6, reltol=1e-6, progress=true)
 plot(sol, idxs=[sys.Cai_sub_SL*1E6, sys.Cai_sub_SR*1E6], tspan=(100, 101), ylabel="nM", lab=["Ca (sub SL)" "Ca (sub SR)"])
 
 # ICaL and INaCa are OK
-# JCa_SR is too low?
+# RyR flux is tiny in NRVM
 @unpack INaCa, ICaL, ICaT, ICab, JCa_SL, JCa_SR = sys
 plot(sol, idxs=[INaCa, ICaL, JCa_SL, JCa_SR], tspan=(100, 101), ylabel="uA/uF or uM/ms")
 
 @unpack Jup, Jrel, Jtr = sys
-plot(sol, idxs=[Jup, Jrel], tspan=(100, 101), ylabel="uA/uF or uM/ms")
+plot(sol, idxs=[Jup, Jtr], tspan=(100, 102), ylabel="uM/ms")
 
 # SR Ca release is reletively small (see JSR)
 plot(sol, idxs=[sys.CaJSR, sys.CaNSR], tspan=(100, 104), ylabel="mM")
+
+plot(sol, idxs=[sys.betaSR], tspan=(100, 104))
+
+prob.ps[sys.ACAP_F]
