b7e791a10e90d972072e5359a23ea51e80784546
Wen Wei Tseng <sosiristseng@gmail.com>
2024-11-16 18:29:44 +0800

    Caffeine factor

--- src/er.jl
+++ src/er.jl
@@ -5,16 +5,17 @@ function get_ser_sys(Cai_sub_SR; fracPLB_CKp=0, fracPLBp=0, RyR_CKp=0.2, V_sub_S
         VNSR = 0.9 * VSR
         VJSR = VSR - VNSR
         # RyR
-        kRyR = 20Hz
+        kRyR = 20Hz # 10 Hz
         kaposRyR = 1000Hz
         kanegRyR = 160Hz
+        rCaffeine = 1.0 # Caffeine factor sensitizes RyR
         # SERCA
         VmaxSR = 0.9996mM*Hz
         KmfSR = 0.5μM
         KmrSR = 3.5mM
         kSRleak = 0.005Hz
         fracPKA_PLBo = 1 - 0.079755
-        ktrCaSR = 50Hz
+        ktrCaSR = 50Hz  # 5Hz
         csqntot = 24.750mM
         Kmcsqn = 0.8mM
     end
@@ -36,7 +37,7 @@ function get_ser_sys(Cai_sub_SR; fracPLB_CKp=0, fracPLBp=0, RyR_CKp=0.2, V_sub_S
     fCKII_PLB = (1 - 0.5 * fracPLB_CKp)  # Max effect: fCKII_PLB=0.5
     fPKA_PLB = ((1 - fracPLBp) / fracPKA_PLBo) * (1 - 0.5531) + 0.5531
     # Select the smaller value (resulting in max reduction of Kmf)
-    Kmfp = KmfSR * min(fCKII_PLB, fPKA_PLB)  #fCKII_PLB
+    Kmfp = KmfSR * min(fCKII_PLB, fPKA_PLB)  # fCKII_PLB
     fSR = (Cai_sub_SR / Kmfp)^2
     rSR = (CaNSR / KmrSR)^2
     kleak = (1 + 5 * RyR_CKp) * kSRleak / 2
@@ -45,7 +46,7 @@ function get_ser_sys(Cai_sub_SR; fracPLB_CKp=0, fracPLBp=0, RyR_CKp=0.2, V_sub_S
         1 ~ PO1RyR + PC1RyR,
         Jrel ~ kRyR * PO1RyR * (CaJSR - Cai_sub_SR),
         KmRyR ~ (3.51 / (1 + exp((CaJSR - 530μM) / 200μM)) + 0.25) * μM,
-        D(PO1RyR) ~ kaposRyR * hil(Cai_sub_SR, KmRyR, 4) * PC1RyR - kanegRyR * PO1RyR,
+        D(PO1RyR) ~ kaposRyR * hil(Cai_sub_SR, KmRyR * rCaffeine, 4) * PC1RyR - kanegRyR * PO1RyR,
         Jup ~ VmaxSR * (fSR - rSR) / (1 + fSR + rSR),
         Jleak ~ kleak * (CaNSR - Cai_sub_SR),
         Jtr ~ ktrCaSR * (CaNSR - CaJSR),
