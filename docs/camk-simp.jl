68c0609d99a2d7da70fe06447f3f793b0c78d13c
Wen Wei Tseng <sosiristseng@gmail.com>
2024-11-27 13:53:08 +0800

    simp

--- docs/camk-simp.jl
+++ docs/camk-simp.jl
@@ -24,16 +24,16 @@ stimend = 300.0
 alg = FBDF()
 @unpack Istim = sys
 callback = build_stim_callbacks(Istim, stimend; period=1, starttime=stimstart)
-prob = ODEProblem(sys, [sys.kCaM4_on => 15Hz / μM, sys.kCaM2C_on => 0.5Hz / μM], tend)
-prob_simp = ODEProblem(sys_simp, [sys_simp.kCaM4_on => 15Hz / μM, sys_simp.kCaM2C_on => 0.5Hz / μM], tend)
+prob = ODEProblem(sys, [], tend)
+prob_simp = ODEProblem(sys_simp, [], tend)
 
 #---
 @time sol = solve(prob, alg; callback, abstol=1e-6, reltol=1e-6)
 @time sol_simp = solve(prob_simp, alg; callback, abstol=1e-6, reltol=1e-6)
 
 #---
-plot(sol, idxs=sys.vm*1000, tspan=(295, 300), label= "Full model", title="Action potential")
-plot!(sol_simp, idxs=sys.vm*1000, tspan=(295, 300), label= "Simplified model", line=:dash)
+plot(sol, idxs=sys.vm*1000, tspan=(295, 300), label= "Full", title="Action potential")
+plot!(sol_simp, idxs=sys.vm*1000, tspan=(295, 300), label= "Simplified", line=:dash)
 
 # Something is wrong
 plot(sol, idxs=sys.CaMKAct, label= "Full model", title="Active CaMKII")
@@ -42,7 +42,7 @@ plot!(sol_simp, idxs=sys_simp.CaMKAct, label= "Simplified model", line=:dash)
 # PCaM too high
 @unpack CaM, KCaM, PCaM, CaMK = sys_simp
 @unpack CaM0, CaM2C, CaM2N, CaM4, CaM0_CaMK, CaM2C_CaMK, CaM2N_CaMK, CaM4_CaMK, CaM0_CaMKP, CaM2C_CaMKP, CaM2N_CaMKP, CaM4_CaMKP = sys_simp
-plot(sol_simp, idxs=[CaM0, CaM2C, CaM2N, CaM4], tspan=(100, 105))
+plot(sol_simp, idxs=[CaM, KCaM, PCaM, CaMK], tspan=(100, 105))
 
 @unpack CaM0, Ca2CaM_C, Ca2CaM_N, Ca4CaM = sys
 plot(sol, idxs=[CaM0, Ca2CaM_C, Ca2CaM_N, Ca4CaM], tspan=(100, 105))
