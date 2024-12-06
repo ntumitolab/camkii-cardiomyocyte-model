0881de1e6ba6e4e598dda3bec77e3c935fd9a50e
Wen Wei Tseng <sosiristseng@gmail.com>
2024-12-06 14:30:12 +0800

    Caffeine at start

--- docs/caffeine-effects.jl
+++ docs/caffeine-effects.jl
@@ -26,8 +26,40 @@ callback = build_stim_callbacks(Istim, stimend; period=1second, starttime=stimst
 callback_caf = CallbackSet(build_stim_callbacks(Istim, stimend; period=1second, starttime=stimstart), PresetTimeCallback(200.0second, add_coffee_affect!))
 
 #---
-sol = solve(prob, alg; callback)
-sol_caf = solve(prob, alg; callback=callback_caf)
+@time sol = solve(prob, alg; callback)
+@time sol_caf = solve(prob, alg; callback=callback_caf)
+
+#---
+plot(sol, idxs=sys.vm, title="Action potential", lab="Ctl")
+plot!(sol_caf, idxs=sys.vm, lab="Caf", tspan=(198second, 205second), ylabel="Voltage (mV)")
+
+#---
+plot(sol, idxs=sys.Cai_sub_SR * 1000, title="Calcium transient (During caffeine addition)", lab="Ctl")
+plot!(sol_caf, idxs=sys.Cai_sub_SR * 1000, tspan=(198second, 205second), lab="Caf", ylabel="Subspace calcium (nM)")
+
+#---
+plot(sol, idxs=sys.PO1RyR, title="RyR open (During caffeine addition)", lab="Ctl")
+plot!(sol_caf, idxs=sys.PO1RyR, tspan=(198second, 205second), lab="Caf", ylabel="Open probability", ylims=(0, 1), xlabel="Time (ms)")
+
+#---
+plot(sol, idxs=sys.Cai_sub_SR * 1000, title="Calcium transient (After caffeine addition)", lab="Ctl", ylabel="Subspace calcium (nM)")
+plot!(sol_caf, idxs=sys.Cai_sub_SR * 1000, tspan=(198second, 205second), lab="Caf", xlabel="Time (ms)")
+
+#---
+plot(sol, idxs=sys.CaJSR, title="SR Calcium (During caffeine addition)", lab="Ctl", ylabel="SR calcium (μM)")
+plot!(sol_caf, idxs=sys.CaJSR, tspan=(198second, 205second), lab="Caf", ylims=(0, 850), xlabel="Time (ms)")
+
+#---
+plot(sol, idxs=sys.Jrel, title="Ca flux", lab="Ctl  (Jrel)")
+plot!(sol_caf, idxs=sys.Jrel, lab="Caf (Jrel)", tspan=(198second, 205second), ylabel="μM/ms", xlabel="Time (ms)")
+
+#---
+plot(sol, idxs=sys.CaMKAct*100, title="Active CaMKII", lab="Ctl")
+plot!(sol_caf, idxs=sys.CaMKAct*100, lab="Caf", ylabel="CaMKII activity (%)", xlabel="Time (ms)")
+
+# Add caffeine in the beginning of the simulation
+prob_caf = ODEProblem(sys, [sys.RyRsensitivity => 10], tend)
+@time sol_caf = solve(prob_caf, alg; callback)
 
 #---
 plot(sol, idxs=sys.vm, title="Action potential", lab="Ctl")
