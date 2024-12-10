f6c5cd49982e6c86fa55b419664ffe5465b00492
Wen Wei Tseng <sosiristseng@gmail.com>
2024-12-10 15:05:29 +0800

    merge sens and simp

--- docs/camk-sens.jl
+++ docs/camk-sens.jl
@@ -1,4 +1,4 @@
-# # Sensitivity of CaMKII system to calcium
+# # CaMKII system analysis
 using ModelingToolkit
 using OrdinaryDiffEq
 using SteadyStateDiffEq
@@ -6,19 +6,18 @@ using DiffEqCallbacks
 using Plots
 using LsqFit
 using CaMKIIModel
-using CaMKIIModel: μM, hil, second
+using CaMKIIModel: μM, hil, second, Hz
 Plots.default(lw=1.5)
 
-# ## Setup CaMKII system
-# Model CaM/Calcium binding only. NO phosphorylation or oxidation.
+# ## CaMKII sensitivity to calcium
+# Model CaM/Calcium binding only. No phosphorylation or oxidation.
 @parameters Ca = 0μM ROS = 0μM
 sys = get_camkii_sys(Ca; ROS, simplify=true)
-equations(sys)
 
 #---
 prob = SteadyStateProblem(sys, [sys.k_phosCaM => 0])
 alg = DynamicSS(Rodas5P())
-ca = logrange(0.1μM, 10μM, length=1001)
+ca = logrange(0.03μM, 10μM, length=1001)
 prob_func = (prob, i, repeat) -> begin
     remake(prob, p=[Ca => ca[i]])
 end
@@ -44,17 +43,18 @@ plot!(ca, extract(sim, sys.Ca2CaM_C_CaMK), lab="Ca2CaM_C_CaMK")
 plot!(ca, extract(sim, sys.Ca2CaM_N_CaMK), lab="Ca2CaM_N_CaMK")
 plot!(ca, extract(sim, sys.Ca4CaM_CaMK), lab="Ca4CaM_CaMK", legend=:topleft, size=(600, 600))
 
-#---
-println("Basal activity without ca is ", sol0[sys.CaMKAct][end])
-plot(ca, extract(sim, sys.CaMKAct), label=false, title="Active CaMKII", ylims=(0, 0.5); xopts...)
+# We excluded CaM0_CaMK (bound but lost all calcium) from the active CaMKII fraction
+CaMKAct = 1 - (sys.CaMK + sys.CaM0_CaMK) / sys.CAMKII_T
+println("Basal activity without ca is ", sol0[CaMKAct][end])
+xdata = ca
+ydata = extract(sim, CaMKAct)
+plot(xdata, ydata, label=false, title="Active CaMKII", ylims=(0, 0.5); xopts...)
 
 # ## Least-square fitting of steady state CaMKII activity
 @. model_camk(x, p) = p[1] * hil(x, p[2], p[4]) + p[3]
-xdata = ca
-ydata = extract(sim, sys.CaMKAct)
-p0 = [0.2, 1μM, 0.02, 2.2]
-lb = [0.0, 1E-6μM, 0.0, 1.0]
-fit = curve_fit(model_camk, xdata, ydata, inv.(ydata), p0; lower=lb, autodiff=:forwarddiff)
+p0 = [0.4, 1μM, 0.0, 2.0]
+lb = [0.0, 0.001μM, 0.0, 1.0]
+fit = curve_fit(model_camk, xdata, ydata, p0; lower=lb, autodiff=:forwarddiff)
 
 # Parameters
 pestim = coef(fit)
@@ -64,16 +64,12 @@ println("Basal activity: ", pestim[3])
 println("Maximal activated activity: ", pestim[1])
 println("Half-saturation Ca concentration: ", pestim[2], " μM")
 println("Hill coefficient: ", pestim[4])
+println("RMSE: ", rmse(fit))
 
-# Loss value
-rmse(fit)
-
-# Fit result
+# Fit result and the original model
 yestim = model_camk.(xdata, Ref(pestim))
 
 p1 = plot(xdata, [ydata yestim], lab=["Full model" "Fitted"], line=[:dash :dot], title="CaMKII activity", legend=:topleft; xopts...)
-p2 = plot(xdata, (yestim .- ydata) ./ ydata .* 100, title="Relative error (%)", xlabel="Ca (μM)", lab=false; xopts...)
-plot(p1, p2, layout=(2, 1), size=(600, 600))
 
 # ## The simplified CaMKII system
 simpsys = CaMKIIModel.get_camkii_simp_sys(Ca; ROS, simplify=true)
@@ -93,7 +89,7 @@ alg = FBDF()
 @unpack Istim = sys
 callback = build_stim_callbacks(Istim, stimend; period=1second, starttime=stimstart)
 prob = ODEProblem(sys, [], tend)
-prob_simp = ODEProblem(sys_simp, [], tend)
+prob_simp = ODEProblem(sys_simp, [sys_simp.kphos_CaMK => 10Hz], tend)
 
 #---
 @time sol = solve(prob, alg; callback)
@@ -105,11 +101,7 @@ plot!(sol_simp, idxs=sys_simp.CaMKAct, label= "Simplified model", xlabel="Time (
 
 #---
 @unpack CaMKB, CaMKP, CaMKA, CaMKA2, CaMK = sys_simp
-plot(sol_simp, idxs=[CaMKB, CaMKP, CaMKA, CaMKA2, CaMK])
-
-#---
-@unpack CaMK, CaMKP, CaMKP2, CaM0_CaMK, Ca2CaM_C_CaMK, Ca2CaM_N_CaMK, Ca4CaM_CaMK, CaM0_CaMKP, Ca2CaM_C_CaMKP, Ca2CaM_N_CaMKP, Ca4CaM_CaMKP = sys
-plot(sol, idxs=[sum([CaM0_CaMK, Ca2CaM_C_CaMK, Ca2CaM_N_CaMK, Ca4CaM_CaMK]), sum([CaM0_CaMKP, Ca2CaM_C_CaMKP, Ca2CaM_N_CaMKP, Ca4CaM_CaMKP]), CaMKP, CaMKP2, CaMK])
+plot(sol_simp, idxs=[CaMKB, CaMKP, CaMKA, CaMKA2, CaMK], legend=:right)
 
 #---
 plot(sol, idxs=sys.CaMKAct, label= "Full model", title="Active CaMKII")
