ebf96808e1cac588a57441b646ed19274770b7c6
Wen Wei Tseng <sosiristseng@gmail.com>
2025-01-13 15:39:46 +0800

    re fit

--- docs/iso-sens.jl
+++ docs/iso-sens.jl
@@ -17,12 +17,10 @@ prob = SteadyStateProblem(sys, [])
 alg = DynamicSS(Rodas5P())
 
 # Log scale for ISO concentration
-iso = logrange(1e-4μM, 3μM, length=1001)
+iso = logrange(1e-4μM, 1μM, length=1001)
 
 #---
-prob_func = (prob, i, repeat) -> begin
-    remake(prob, p=[ISO => iso[i]])
-end
+prob_func = (prob, i, repeat) -> remake(prob, p=[ISO => iso[i]])
 trajectories = length(iso)
 sol = solve(prob, alg; abstol=1e-10, reltol=1e-10) ## warmup
 sim = solve(EnsembleProblem(prob; prob_func, safetycopy=false), alg; trajectories, abstol=1e-10, reltol=1e-10)
@@ -36,12 +34,6 @@ rmse(fit) = sqrt(sum(abs2, fit.resid) / length(fit.resid))
 #---
 xopts = (xlims=(iso[begin], iso[end]), minorgrid=true, xscale=:log10, xlabel="ISO (μM)",)
 plot(iso, extract(sim, sys.cAMP); lab="cAMP", ylabel="Conc. (μM)", legend=:topleft, xopts...)
-
-#---
-plot(iso, [extract(sim, sys.PKACI) extract(sim, sys.PKACII)], legend=:topleft, lab=["PKACI" "PKACII"]; xopts...)
-
-#---
-plot(iso, [extract(sim, sys.I1) extract(sim, sys.I1p) extract(sim, sys.I1p_PP1)], legend=:topleft, lab=["I1" "I1p" "I1p_PP1"]; xopts...)
 
 #---
 plot(iso, extract(sim, sys.PKACI / sys.RItot); lab="PKACI", ylabel="Activation fraction")
@@ -139,47 +131,6 @@ p1 = plot(xdata, [ydata ypred], lab=["Full model" "Fitted"], line=[:dash :dot], 
 p2 = plot(xdata, (ypred .- ydata) ./ ydata .* 100; title="PLBp error (%)", lab=false, xopts...)
 plot(p1, p2, layout=(2, 1), size=(600, 600))
 
-# Second try: analytic PLB from the quadratic equation
-function plbp_analytic(iso)
-    PLBtot = 106μM
-    PKACItot = 1.18μM
-    PP1tot = 0.89μM
-    k_PKA_PLB = 54Hz / μM
-    Km_PKA_PLB = 21μM
-    k_PP1_PLB = 8.5Hz / μM
-    Km_PP1_PLB = 7.0μM
-    PKACI_basal = 0.0734  ## basal activity
-    PKACI_activated = 0.1995
-    PKACI_KM = 0.0139μM
-    PP1_basal = 0.8927
-    PP1_activated = 0.0492
-    PP1_KI = 0.00637μM
-
-    ## Solve for Vf * x / (x + k1) = Vr * (1 - x) / (1 - x + k2)
-    PKACI = PKACItot * (PKACI_basal + PKACI_activated * hil(iso, PKACI_KM))
-    PP1 = PP1tot * (PP1_basal + PP1_activated * hilr(iso, PP1_KI))
-
-    Vf = k_PKA_PLB * PKACI
-    k1 = Km_PKA_PLB / PLBtot
-    Vr = k_PP1_PLB * PP1
-    k2 = Km_PP1_PLB / PLBtot
-    A = 1 - (Vf / Vr)
-    B = (Vf / Vr) * (1 + k2) + (k1 - 1)
-    C = -k1
-    plb = ifelse(A ≈ 0.0, k1 / (k1 + k2), (-B + sqrt(B^2 - 4 * A * C)) / 2A)
-    plbp = 1 - plb
-end
-
-ypred = plbp_analytic.(xdata)
-fit = (resid=(ypred .- ydata) ./ ydata,)
-
-p1 = plot(xdata, [ydata ypred], lab=["Full model" "Quadratic"], line=[:dash :dot], title="PLBp", legend=:topleft; xopts...)
-p2 = plot(xdata, fit.resid .* 100; title="PLBp error (%)", lab=false, xopts...)
-plot(p1, p2, layout=(2, 1), size=(600, 600))
-
-#---
-println("RMSE: ", rmse(fit))
-
 # ## Fitting PLMp
 xdata = iso
 ydata = extract(sim, sys.PLMp / sys.PLMtotBA)
@@ -238,18 +189,17 @@ ydata = extract(sim, sys.LCCap / sys.LCCtotBA)
 plot(xdata, ydata, title="LCCap fraction", lab=false; xopts...)
 
 #---
-@. model_lcc(x, p) = p[1] * hil(x, p[2], p[3]) + p[4]
-p0 = [0.8, 1e-2μM, 1.0, 0.1]
-lb = [0.1, 1e-9μM, 0.5, 0.0]
+@. model_lcc(x, p) = p[1] * hil(x, p[2]) + p[3]
+p0 = [0.8, 1e-2μM, 0.1]
+lb = [0.1, 1e-9μM, 0.0]
 fit = curve_fit(model_lcc, xdata, ydata, p0; lower=lb, autodiff=:forwarddiff)
 pestim = coef(fit)
 
 #---
 println("LCCap")
-println("Basal activity: ", pestim[4])
+println("Basal activity: ", pestim[3])
 println("Activated activity: ", pestim[1])
 println("Michaelis constant: ", pestim[2], " μM")
-println("Hill coefficient: ", pestim[3])
 println("RMSE: ", rmse(fit))
 
 #---
@@ -264,18 +214,16 @@ ydata = extract(sim, sys.LCCbp / sys.LCCtotBA)
 plot(xdata, ydata, title="LCCbp fraction", lab=false; xopts...)
 
 #---
-@. model_lcc(x, p) = p[1] * hil(x, p[2], p[3]) + p[4]
-p0 = [0.8, 1e-2μM, 1.0, 0.1]
-lb = [0.1, 1e-9μM, 0.5, 0.0]
+p0 = [0.8, 1e-2μM, 0.1]
+lb = [0.1, 1e-9μM, 0.0]
 fit = curve_fit(model_lcc, xdata, ydata, p0; lower=lb, autodiff=:forwarddiff)
 pestim = coef(fit)
 
 #---
 println("LCCbp")
-println("Basal activity: ", pestim[4])
+println("Basal activity: ", pestim[3])
 println("Activated activity: ", pestim[1])
 println("Michaelis constant: ", pestim[2], " μM")
-println("Hill coefficient: ", pestim[3])
 println("RMSE: ", rmse(fit))
 
 #---
@@ -290,18 +238,17 @@ ydata = extract(sim, sys.KURp / sys.IKurtotBA)
 plot(xdata, ydata, title="LCCbp fraction", lab=false; xopts...)
 
 #---
-@. model_kur(x, p) = p[1] * hil(x, p[2], p[3]) + p[4]
-p0 = [0.8, 1e-2μM, 1.0, 0.1]
-lb = [0.1, 1e-9μM, 0.5, 0.0]
+@. model_kur(x, p) = p[1] * hil(x, p[2]) + p[3]
+p0 = [0.8, 1e-2μM, 0.1]
+lb = [0.1, 1e-9μM, 0.0]
 fit = curve_fit(model_kur, xdata, ydata, p0; lower=lb, autodiff=:forwarddiff)
 pestim = coef(fit)
 
 #---
 println("KURp")
-println("Basal activity: ", pestim[4])
+println("Basal activity: ", pestim[3])
 println("Activated activity: ", pestim[1])
 println("Michaelis constant: ", pestim[2], " μM")
-println("Hill coefficient: ", pestim[3])
 println("RMSE: ", rmse(fit))
 
 #---
