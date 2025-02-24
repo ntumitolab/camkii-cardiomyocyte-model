2b703171169127669a0dd6c7db620c316d114778
Wen Wei Tseng <sosiristseng@gmail.com>
2025-02-24 23:53:32 +0800

    kg (#53)

--- docs/ros-effects.jl
+++ docs/ros-effects.jl
@@ -1,6 +1,6 @@
 # # ROS effects
 using ModelingToolkit
-using OrdinaryDiffEq
+using DifferentialEquations
 using Plots
 using CSV
 using DataFrames
--- docs/u0.jl
+++ docs/u0.jl
@@ -1,7 +1,6 @@
 # # Initial conditions
 using ModelingToolkit
-using OrdinaryDiffEq
-using SteadyStateDiffEq
+using DifferentialEquations
 using Plots
 using CaMKIIModel
 Plots.default(lw=2)
