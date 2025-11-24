42f5aa7fcfccbea991d5bab798685fd6c3ea0f0c
Wen Wei Tseng <sosiristseng@gmail.com>
2025-11-24 16:38:36 +0800

    rm SciMLBase

--- src/CaMKIIModel.jl
+++ src/CaMKIIModel.jl
@@ -3,7 +3,7 @@ module CaMKIIModel
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
 using NaNMath
-using SciMLBase  # CallbackSet
+using OrdinaryDiffEq
 using DiffEqCallbacks
 
 export get_camkii_sys, get_bar_sys, build_neonatal_ecc_sys, build_stim_callbacks, get_bar_sys_reduced
