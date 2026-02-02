fa55e8a8d08be6583a7ea03062f0d883194fab58
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-02 23:14:48 +0800

    split

---
+++ docs/bcl-freq.jl
@@ -0,0 +1,46 @@
+# # Pacing frequency
+# ## Experiments
+
+freqdf = CSV.read(joinpath(@__DIR__, "data/CaMKAR-freq.csv"), DataFrame)
+ts = 0:5:205
+onehz = freqdf[!, "1Hz (Mean)"]
+onehz_error = freqdf[!, "1Hz (SD)"] ./ sqrt.(freqdf[!, "1Hz (N)"])
+twohz = freqdf[!, "2Hz (Mean)"]
+twohz_error = freqdf[!, "2Hz (SD)"] ./ sqrt.(freqdf[!, "2Hz (N)"])
+
+plot(ts, onehz, yerr=onehz_error, lab="1 Hz", color=:blue, markerstrokecolor=:blue)
+plot!(ts, twohz, yerr=twohz_error, lab="2 Hz", color=:red, markerstrokecolor=:red)
+plot!(title="Experiment", xlabel="Time (s)", ylabel="CaMKII activity (AU)")
+
+#---
+savefig("pacing-frequency-exp.pdf")
+
+# ## Simulations
+
+# ### CaMKII activity
+tend = 205.0second
+prob = ODEProblem(sys, [], tend)
+stimstart = 30.0second
+stimend = 120.0second
+callback = build_stim_callbacks(Istim, stimend; period=1second, starttime=stimstart)
+sol1 = solve(prob, alg; callback)
+
+callback2 = build_stim_callbacks(Istim, stimend; period=0.5second, starttime=stimstart)
+sol2 = solve(prob, alg; callback=callback2)
+idxs = (sys.t / 1000, sys.CaMKAct * 100)
+
+plot(sol1, idxs=idxs, lab="1 Hz", color=:blue)
+plot!(sol2, idxs=idxs, lab="2 Hz", color=:red)
+plot!(title="Simulation", xlabel="Time (s)", ylabel="CaMKII activity (%)")
+
+#---
+savefig("pacing-frequency-sim.pdf")
+
+# ## Phosphorylated fraction
+idxs = (sys.t / 1000, (sys.CaMKP + sys.CaMKA + sys.CaMKA2) * 100)
+plot(sol1, idxs=idxs, lab="1 Hz", color=:blue)
+plot!(sol2, idxs=idxs, lab="2 Hz", color=:red)
+plot!(title="Simulation", xlabel="Time (s)", ylabel="Phosphorylated CaMKII (%)")
+
+#---
+savefig("pacing-frequency-phos.pdf")
