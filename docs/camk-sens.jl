d1497c85c3f49d25af81bcb5f165a94956b0b941
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-02 23:31:47 +0800

    Polynomial fitting

--- docs/camk-sens.jl
+++ docs/camk-sens.jl
@@ -19,14 +19,13 @@ Modeling CaM-Cax binding to CaMKII only. No phosphorylation or oxidation reactio
 # Physiological cytosolic calcium concentrations ranges from 30nM to 10μM.
 ca = exp10.(range(log10(0.03μM), log10(10μM), 1001))
 prob_func = (prob, i, repeat) -> (prob.ps[Ca] = ca[i]; prob)
-alg = DynamicSS(Rodas5P())
-@time "Solve problem" sim = solve(EnsembleProblem(prob; prob_func), alg; trajectories=length(ca), abstol=1e-10, reltol=1e-10)
+alg = DynamicSS(KenCarp47())
+sol0 = solve(prob, alg) ## warmup
+@time "Solve problem" sim = solve(EnsembleProblem(prob; prob_func), alg; trajectories=length(ca))
 
 #---
 """Extract values from ensemble simulations by a symbol"""
 extract(sim, k) = map(s -> s[k], sim)
-"""Calculate Root Mean Square Error (RMSE)"""
-rmse(fit) = sqrt(sum(abs2, fit.resid) / length(fit.resid))
 
 # Status of the CaMKII system across a range of calcium concentrations.
 xopts = (xlabel="Ca (μM)", xscale=:log10, minorgrid=true, xlims=(ca[1], ca[end]))
@@ -46,7 +45,7 @@ end
 savefig("camkii_cam_binding.pdf")
 
 # Active CaMKII is defined as CaMKII bound to CaMCax
-CaMKAct = 1 - (sys.CaMK + sys.CaM0_CaMK) / sys.CAMKII_T
+CaMKAct = 1 - (sys.CaMK) / sys.CAMKII_T
 println("Basal activity with 30nM Ca is ", sol0[CaMKAct][end])
 xdata = ca
 ydata = extract(sim, CaMKAct)
@@ -70,7 +69,6 @@ p0 = [0.4, 1μM, 0.0, 2.0]
 prob = NonlinearCurveFitProblem(model_camk_single_hill, p0, xdata, ydata)
 @time fit = solve(prob)
 
-#---
 println("Basal activity: ", fit.u[3])
 println("Maximal activated activity: ", fit.u[1])
 println("Half-saturation Ca concentration: ", fit.u[2], " μM")
@@ -78,7 +76,7 @@ println("Hill coefficient: ", fit.u[4])
 println("RMSE: ", mse(fit) |> sqrt)
 
 # Fit result and the original
-p1 = plot(xdata, [ydata predict(fit)], lab=["Full model" "Fitted"], line=[:dash :dot], title="Single Hill function fit", legend=:topleft, ylabel="Bound CaMKII"; xopts...)
+plot(xdata, [ydata predict(fit)], lab=["Full model" "Fitted"], line=[:dash :dot], title="Single Hill function fit", legend=:topleft, ylabel="Bound CaMKII"; xopts...)
 
 #===
 ### New model
@@ -91,11 +89,9 @@ $$
 ===#
 model_camk(p, x) = @. p[1] * hil(x, p[2], 2) + p[3] * hil(x, p[4], 4) + p[5]
 p0 = [0.4, 1μM, 0.2, 1μM, 0.0]
-lb = [0.0, 0.001μM, 0.0, 0.001μM, 0.0]
 prob = NonlinearCurveFitProblem(model_camk, p0, xdata, ydata)
 @time fit = solve(prob)
 
-#---
 println("Basal activity: ", fit.u[5])
 println("Maximal activity by 2 Ca binding): ", fit.u[1])
 println("Half saturation concentration for 2 Ca binding: ", fit.u[2], " μM")
@@ -104,7 +100,19 @@ println("Half saturation concentration for 4 Ca binding: ", fit.u[4], " μM")
 println("RMSE: ", mse(fit) |> sqrt)
 
 #---
-p1 = plot(xdata, [ydata predict(fit)], lab=["Full model" "Fitted"], line=[:dash :dot], title="Dual Hill function fit", legend=:topleft, ylabel="Bound CaMKII"; xopts...)
+plot(xdata, [ydata predict(fit)], lab=["Full model" "Fitted"], line=[:dash :dot], title="Dual Hill function fit", legend=:topleft, ylabel="Bound CaMKII"; xopts...)
 
 #---
 savefig("camkii_act_fit.pdf")
+
+# ### Polynomial fitting
+prob = CurveFitProblem(xdata, ydata)
+alg = RationalPolynomialFitAlgorithm(num_degree=2, den_degree=2)
+@time sol = solve(prob, alg)
+
+println("Numerator coefficients: ", sol.u[1:3])
+println("Denominator coefficients: ", vcat(1.0, sol.u[4:5]))
+println("RMSE: ", mse(sol) |> sqrt)
+
+#---
+plot(xdata, [ydata sol.(xdata)], lab=["Full model" "Fitted"], line=[:dash :dot], title="Rational polynomial fit", legend=:topleft, ylabel="Bound CaMKII"; xopts...)
