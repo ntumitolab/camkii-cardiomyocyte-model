b7359a3824c7431c535803f4e27c81361d12f081
Wen Wei Tseng <sosiristseng@gmail.com>
2026-02-23 22:03:14 +0800

    Sens (#154)
    * SciMLSensitivity
    
    * rename
    
    * rename
    
    * pkg
    
    * PKG
    
    * wip
    
    * pkg
    
    * Revert "pkg"
    
    This reverts commit 7351ccc69a69490af2e028c5de5d8132a8f04c78.
    
    * componentarrays
    
    * wip
    
    * jl-112
    
    * iso-simp
    
    * wip
    
    * relative sensitivity analysis
    
    * pkg

---
+++ docs/camki-sens.jl
@@ -0,0 +1,41 @@
+# # Sensitiviy analysis of the CaMKII system
+# Load packages
+using ModelingToolkit
+using OrdinaryDiffEq, SteadyStateDiffEq
+using CaMKIIModel
+using CaMKIIModel: ms, Hz, second
+import SciMLSensitivity as SMS
+import ForwardDiff as FD
+using Plots
+Plots.default(lw=2)
+
+# ## Setup the ODE system
+# Electrical stimulation starts at `t`=100 sec and ends at `t`=300 sec.
+@time @mtkcompile sys = build_neonatal_ecc_sys()
+tend = 500.0*1000ms
+@time prob = ODEProblem(sys, [], tend)
+stimstart = 100.0*1000ms
+stimend = 300.0*1000ms
+@unpack Istim = sys
+
+# ## 1Hz pacing
+callback = build_stim_callbacks(Istim, stimend; period=1*1000ms, starttime=stimstart)
+@time sol = solve(prob, KenCarp47(); callback, reltol = 1e-8, abstol = 1e-8);
+# Sensitivity analysis of the solution at `t`=300 sec against all parameters.
+@unpack r_CaMK, kb_CaMKP, kphos_CaMK, kdeph_CaMK, k_P1_P2, k_P2_P1 = sys
+
+function sens(x; t=300.0*1000ms)
+    _prob = remake(prob, p=[sys.r_CaMK => x[1], sys.kb_CaMKP => x[2], sys.kphos_CaMK => x[3], sys.kdeph_CaMK => x[4], sys.k_P1_P2 => x[5], sys.k_P2_P1 => x[6]])
+    sol = solve(_prob, KenCarp47(); callback, reltol = 1e-8, abstol = 1e-8, saveat = [t])
+    sol(t, idxs = sys.CaMKAct)
+end
+
+#---
+x = [3Hz, inv(3second), 5Hz, inv(6second), inv(60second), inv(15second)];
+@time dx = FD.gradient(sens, x)
+
+#---
+println("Relative sensitivity of CaMKII activity at 1Hz pacing for 300 sec to parameters:")
+for (i, k) in enumerate([r_CaMK, kb_CaMKP, kphos_CaMK, kdeph_CaMK, k_P1_P2, k_P2_P1])
+     println("$k : ", dx[i] * x[i] / sol(300.0*1000ms, idxs=sys.CaMKAct))
+end
