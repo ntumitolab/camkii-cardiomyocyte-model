3d9487d3f8a026c6178979b04345cfc734babb17
Wen Wei Tseng <sosiristseng@gmail.com>
2026-04-01 17:41:45 +0800

    wip

--- Model/src/isoproterenol.jl
+++ Model/src/isoproterenol.jl
@@ -1,5 +1,5 @@
 "Beta-adrenergic system activated by isoproterenol"
-function get_bar_sys(ATP=5000μM, ISO=0μM; name=:bar_sys, simplify=false)
+function get_bar_sys(ATP=5000μM, ISO=0μM; name=:bar_sys)
     @parameters begin
         b1ARtot = 5.28nM
         Gstot = 3.83μM
@@ -258,7 +258,7 @@ function get_bar_sys(ATP=5000μM, ISO=0μM; name=:bar_sys, simplify=false)
 
     rateeqs = [D(s) ~ rates[s] for s in sts]
     sys = System([rateeqs; conservedeqs; obseqs], t; name)
-    return simplify ? mtkcompile(sys) : sys
+    return sys
 end
 
 function get_bar_eqs_reduced(ISO=0μM)
