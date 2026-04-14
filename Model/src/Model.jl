e9e970b10ae4bf9f9b2feaa26a6d115b7e477aa6
Wen Wei Tseng <sosiristseng@gmail.com>
2026-04-14 20:48:47 +0800

    precompiletools seems not effective

--- Model/src/Model.jl
+++ Model/src/Model.jl
@@ -5,7 +5,6 @@ using ModelingToolkit: t_nounits as t, D_nounits as D
 using NaNMath
 using OrdinaryDiffEq
 using DiffEqCallbacks
-using PrecompileTools: @setup_workload, @compile_workload
 
 export build_neonatal_ecc_sys, build_stim_callbacks
 
@@ -19,9 +18,8 @@ include("ik.jl")
 include("ina.jl")
 include("er.jl")
 include("ecc_neonatal.jl")
-include("precompile.jl")
 
 const DEFAULT_SYS = build_neonatal_ecc_sys() |> mtkcompile
-const DEFAULT_PROB = ODEProblem(DEFAULT_SYS, [], (0.0, 200.0second))
+const DEFAULT_PROB = ODEProblem(DEFAULT_SYS, [], (0.0, 200.0second), eval_expression = true, eval_module = @__MODULE__)
 
 end # module Model
