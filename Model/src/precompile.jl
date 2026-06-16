ae4f37d342f1b7e00fddff406f88db0a98f02388
Wen Wei Tseng <sosiristseng@gmail.com>
2026-06-16 21:26:47 +0800

    CI

--- Model/src/precompile.jl
+++ Model/src/precompile.jl
@@ -1,11 +1,16 @@
+using CSV
+using DataFrames
+using DiffEqCallbacks
+using DifferentialEquations
+using ModelingToolkit
+using NaNMath
 using OrdinaryDiffEqSDIRK
-using SteadyStateDiffEq
 using Plots
+using SteadyStateDiffEq
 
 @compile_workload begin
     sys = build_neonatal_ecc_sys() |> mtkcompile
     prob = ODEProblem(sys, [], (0.0, 200.0second))
     sprob = SteadyStateProblem(sys, [])
     sol = solve(prob, KenCarp4())
-    plot(sol, idxs=sys.CaMKAct)
 end
