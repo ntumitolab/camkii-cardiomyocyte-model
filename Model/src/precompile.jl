fbbaad703cca17f79ce8da885a91e3a75dd086d7
Wen-Wei Tseng <sosiristseng@gmail.com>
2026-09-17 09:09:23 +0000

    precompile more

--- Model/src/precompile.jl
+++ Model/src/precompile.jl
@@ -1,6 +1,12 @@
-using PrecompileTools: @compile_workload, @recompile_invalidations
+using PrecompileTools: @compile_workload, @recompile_invalidations, @setup_workload
 
-@compile_workload begin
-    DEFAULT_SYS = build_neonatal_ecc_sys() |> mtkcompile
-    DEFAULT_PROB = ODEProblem(DEFAULT_SYS, [], (0.0, 100.0second))
+@setup_workload begin
+    @parameters Ca = 0μM ROS = 0μM
+    @parameters ATP = 5000μM ISO = 0μM
+    @compile_workload begin
+        b1arsys = get_bar_sys(ATP, ISO) |> mtkcompile
+        camsys = get_camkii_sys(; Ca=Ca, ROS=ROS) |> mtkcompile
+        DEFAULT_SYS = build_neonatal_ecc_sys() |> mtkcompile
+        DEFAULT_PROB = ODEProblem(DEFAULT_SYS, [], (0.0, 100.0second))
+    end
 end
